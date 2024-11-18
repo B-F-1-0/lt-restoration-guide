a66ddbee0a78c0d073aad4019b5e90b00d6dc87b
BF10 <jluc1485410@gmail.com>
2024-11-18 13:00:42 -0500

    Updating to Bunny.net font in CSS for flexibility

--- createnav.js
+++ createnav.js
@@ -3,7 +3,7 @@ const pageFooter = document.createElement('footer');
 
 pageHeader.innerHTML = `
 <h1>The <em>Looney Tunes</em> Restoration Guide</h1>
-<p><a target="_parent" href="\/index.html">Home</a> • <a target="_parent" href="\/history.html">History</a> • <a target="_parent" href="\/collectorsguide.html">Collector's Guide</a> • <a target="_parent" href="\/excel.html">Excel Guide</a> • <a target="_parent" href="\/about.html">About</a></p>`;
+<p><a target="_parent" href="\/">Home</a> • <a target="_parent" href="\/history.html">History</a> • <a target="_parent" href="\/collectorsguide.html">Collector's Guide</a> • <a target="_parent" href="\/excel.html">Excel Guide</a> • <a target="_parent" href="\/about.html">About</a></p>`;
 
 pageFooter.innerHTML = `Copyright &copy; 2024 • Looney Tunes, Merrie Melodies, and its related characters are the exclusive properties of Warner Bros., a Warner Bros. Discovery company. This website is not affiliated with Warner Bros. • <a target="_parent" href="\/glossary.html">Glossary</a> • <a target="_parent" href="\/comment.html">Comments?</a>`;
 
---
+++ homemedia/tableentries.js
@@ -0,0 +1,6 @@
+const homeMediaTableEntry = {
+    releaseName: "",
+    releaseDate: Date("2000-01-01"),
+    cartoons: 0,
+    print: false,
+}
